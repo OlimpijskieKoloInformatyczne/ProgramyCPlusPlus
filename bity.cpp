6fb3eab4d431a4a0708ba71000fde26c84ac6b06
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-01-30 19:55:30 +0100

    Add files via upload

--- bity.cpp
+++ bity.cpp
@@ -7,13 +7,15 @@ int main() {
  liczba1 = 45;
  liczba2 = 14;
 
+if ((a==b) || (a>5))
+
  wynik_and = liczba1 & liczba2; //AND 
  cout << wynik_and << endl;
  
- wynik_or = liczba1 | liczba2; //AND 
+ wynik_or = liczba1 | liczba2; //OR 
  cout << wynik_or << endl;
 
- wynik_xor = liczba1 ^ liczba2; //AND 
+ wynik_xor = liczba1 ^ liczba2; //XOR 
  cout << wynik_xor << endl;
 
  return 0;
