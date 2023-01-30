a0c58d53a919356357d72bfa923eb9658b3f0a81
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-01-30 18:45:02 +0100

    Add files via upload

--- bity.cpp
+++ bity.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
- int liczba = 5;
+ int liczba = 27;
  cout << liczba << endl; 
 
+ liczba = 27;
  liczba = liczba << 1;
- 
+ cout << liczba << endl;
+
+ liczba = 27;
+ liczba = liczba << 2;
  cout << liczba << endl; 
  
  return 0;
