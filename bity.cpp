4a8108f5d788b63f07e43a63926a28df47ded3a9
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-01-30 19:37:03 +0100

    Add files via upload

--- bity.cpp
+++ bity.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main() {
- int liczba1, liczba2, wynik_and;
+ int liczba1, liczba2, wynik_and, wynik_or, wynik_xor;
 
  liczba1 = 45;
  liczba2 = 14;
 
  wynik_and = liczba1 & liczba2; //AND 
- 
  cout << wynik_and << endl;
  
+ wynik_or = liczba1 | liczba2; //AND 
+ cout << wynik_or << endl;
+
+ wynik_xor = liczba1 ^ liczba2; //AND 
+ cout << wynik_xor << endl;
+
  return 0;
 }
