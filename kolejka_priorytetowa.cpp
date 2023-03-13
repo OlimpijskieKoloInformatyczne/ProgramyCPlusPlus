f671be5987dd66430208863f3fe5e2ad889111e8
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-03-13 18:44:20 +0100

    Add files via upload

--- kolejka_priorytetowa.cpp
+++ kolejka_priorytetowa.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main() {
+// priority_queue<int, vector<int>, greater<int> > wplaty_na_gorze_MIN;
+
  priority_queue< int > wplaty_na_gorze_MAX;
- priority_queue<int, vector<int>, greater<int> > wplaty_na_gorze_MIN;
  int datek;
 
  datek = 40;
@@ -13,9 +14,22 @@ int main() {
  wplaty_na_gorze_MAX.push(datek); 
  datek = 30;
  wplaty_na_gorze_MAX.push(datek); 
+
+ wplaty_na_gorze_MAX.pop(); //usun MAX 
+ wplaty_na_gorze_MAX.pop(); //usun MAX 
+ wplaty_na_gorze_MAX.pop(); //usun MAX 
+ if (wplaty_na_gorze_MAX.size() > 0) {
+    wplaty_na_gorze_MAX.pop(); //usun MAX 
+ } else {
+    cout << "DLACZEGO kazesz mi usunac element jak kolejka PUSTA!!!!!!!!!!!" << endl; 
+ }
+ if (wplaty_na_gorze_MAX.size() > 0) {
+    cout <<  wplaty_na_gorze_MAX.top() << endl;
+ } else {
+    cout <<  "Sorry - nie ma najwiekszego!!!" << endl;
+ }
+//nasza kolejka jest pusta 
  
- cout << wplaty_na_gorze_MAX.top() << endl;
- 
+ cout << wplaty_na_gorze_MAX.pop();
  return 0;
 }
-
