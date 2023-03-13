e32def5e6e132568d086e3c4df6552e1c2af80a1
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-03-13 19:04:11 +0100

    Add files via upload

--- kolejka_priorytetowa.cpp
+++ kolejka_priorytetowa.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
+#include <string>
 #include <queue>
 using namespace std;
 
 int main() {
-// priority_queue<int, vector<int>, greater<int> > wplaty_na_gorze_MIN;
-
- priority_queue< int > wplaty_na_gorze_MAX;
- int datek;
-
- datek = 40;
- wplaty_na_gorze_MAX.push(datek); 
- datek = 80;
- wplaty_na_gorze_MAX.push(datek); 
- datek = 30;
- wplaty_na_gorze_MAX.push(datek); 
-
- wplaty_na_gorze_MAX.pop(); //usun MAX 
- wplaty_na_gorze_MAX.pop(); //usun MAX 
- wplaty_na_gorze_MAX.pop(); //usun MAX 
- if (wplaty_na_gorze_MAX.size() > 0) {
-    wplaty_na_gorze_MAX.pop(); //usun MAX 
- } else {
-    cout << "DLACZEGO kazesz mi usunac element jak kolejka PUSTA!!!!!!!!!!!" << endl; 
- }
- if (wplaty_na_gorze_MAX.size() > 0) {
-    cout <<  wplaty_na_gorze_MAX.top() << endl;
- } else {
-    cout <<  "Sorry - nie ma najwiekszego!!!" << endl;
- }
-//nasza kolejka jest pusta 
+ priority_queue<string> lista;
  
- cout << wplaty_na_gorze_MAX.pop();
+ lista.push("byc");
+ lista.push("albo"); 
+ lista.push("nie"); 
+ lista.push("byc"); 
+ 
+ while (lista.size() > 0) {
+    cout << lista.top() << " ";
+    lista.pop();
+ }
+
  return 0;
 }
