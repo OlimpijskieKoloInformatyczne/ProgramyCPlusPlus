90459d16d9bbe8e23d24f108fe61fb40c9706365
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-04-24 19:04:20 +0200

    Add files via upload

---
+++ lower_bound_testy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>  
+using namespace std;
+
+const int ILE_WARTOSCI = 2;
+const int ILE_PYTAN = 10;
+
+int main() {
+ vector<int> wartosci;
+ vector<int>::iterator it;
+ int i, szukana_wartosc, index;
+ 
+ wartosci.resize(ILE_WARTOSCI+1);
+
+ for (i=0; i<ILE_WARTOSCI; ++i)
+    wartosci[i] = i;
+ wartosci[ILE_WARTOSCI] = ILE_WARTOSCI+100; //straznik
+
+ sort(wartosci.begin(), wartosci.end() );
+
+ for (i=0; i<ILE_PYTAN; ++i) {
+    szukana_wartosc = i;
+    it = lower_bound (wartosci.begin(), wartosci.begin()+ILE_WARTOSCI, szukana_wartosc);
+    index = it - wartosci.begin();
+ }
+ 
+ return 0;
+}
