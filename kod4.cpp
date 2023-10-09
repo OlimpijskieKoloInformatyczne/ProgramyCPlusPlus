e945f9b8960813927d48e64ef4b32d686420e719
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-10-09 18:52:07 +0200

    Add files via upload

---
+++ kod4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int ocena;
+
+//jedno rowna sie podstawienie 
+ ocena = 6;
+ 
+ //dwa rowna sie porownanie
+ if (ocena == 6) 
+    cout << "Kujon" << endl;
+ 
+ 
+ 
+ return 0;
+}
