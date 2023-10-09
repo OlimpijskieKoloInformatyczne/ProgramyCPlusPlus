405b2be76cd39e96fb3f11ff550d9960e1ee842f
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-10-09 17:57:45 +0200

    Add files via upload

---
+++ Kod1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int stan_konta, kwota_przelewu;
+
+ stan_konta = 10;
+ kwota_przelewu = 5;
+
+ cout << stan_konta << " " << kwota_przelewu;
+
+ return 0;
+}
