44aae1dda3eb2ed28dc7a596a3adae74da985964
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-10-23 18:51:23 +0200

    Add files via upload

---
+++ petla1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int poczatek_wypisywania, i;
+ 
+ cin >> poczatek_wypisywania;
+ 
+ for (i=poczatek_wypisywania; i>=1; --i)
+    cout << i << " "; 
+
+ cout << endl;
+ cout << "NOWY ROK!!" << endl;
+ return 0;
+}
